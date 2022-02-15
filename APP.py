b467102194304b5f352bf23327572133e522b924
Adriatik Mehmeti <45045775+AdriatikMehmeti@users.noreply.github.com>
2022-02-15 04:02:00 +0100

    Update APP.py

--- APP.py
+++ APP.py
@@ -6,31 +6,34 @@ from datetime import datetime
 
 def main():
     aab = AAB()
-    aab._config_check()
     aab._aab_connect()
-
     exams = aab._aab_exam_results()
+
+    data = []
+
     if exams and len(exams) > 0:
+        for i in exams: data.append(f"Landa: {i.get('subjectName')} - Nota: {i.get('grade')} - Data: {i.get('data')}")
+        data = aab.update_data(data)
+
+    if data and len(data) > 0:
 
         message = ''
-        data = []
 
-        for i in exams: data.append(i.get('lenda'))
+        for i in data: message += f"{i}\n"
 
-        for i in AAB().update_data(data): message += f"Lenda: {i} \n"
-
-        EMAIL.EMAIL().send_email(f'Exam Results Updated{datetime.now().strftime(AAB()._date_format)}', message)
+        EMAIL.EMAIL().send_email(f'Exam Results Updated{datetime.now().strftime(aab._date_format)}', message)
         print(f"\n{text_color.OKBLUE}[+] AAB eservice new exam results found {text_color.ENDC}")
-        aab.Have_Errors()
+        
     elif(exams == False):
 
-        EMAIL.EMAIL().send_email(f'Eservice Auth Error {datetime.now().strftime(AAB()._date_format)}')
+        EMAIL.EMAIL().send_email(f'Eservice Auth Error {datetime.now().strftime(aab._date_format)}')
         print(f"\n{text_color.FAIL}[!] AAB eservice Auth Error {text_color.ENDC}")
-        aab.Have_Errors()
+        
     else:
-        EMAIL.EMAIL().send_email(f'Not found new results {datetime.now().strftime(AAB()._date_format)}')
+        EMAIL.EMAIL().send_email(f'Not found new results {datetime.now().strftime(aab._date_format)}')
         print(f"\n{text_color.OKGREEN}[-] AAB eservice no change was found {text_color.ENDC}")
-        aab.Have_Errors()
+
+    aab.Have_Errors()
 
 if __name__ == "__main__":
     main()
